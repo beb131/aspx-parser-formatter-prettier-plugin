f3f260371e3e3223aa986622e0588cfc29e358dc
Brandon Bachrach <brandon.bachrach@gmail.com>
2020-10-11 23:02:47 -0400

    Initial Push

--- src/index.js
+++ src/index.js
@@ -1,72 +1,72 @@
-const parser = require('toml/lib/parser')
+const parser = require("../parser/generator");
 const {
   doc: {
-    builders: { concat, hardline }
-  }
-} = require('prettier')
+    builders: { concat, hardline, group, indent, line, join, softline },
+  },
+} = require("prettier");
 
 const languages = [
   {
-    extensions: ['.toml'],
-    name: 'TOML',
-    parsers: ['toml-parse']
-  }
-]
+    extensions: [".aspx", ".ascx", ".inc"],
+    name: "ASPX",
+    parsers: ["aspx-parse"],
+  },
+];
 
 const parsers = {
-  'toml-parse': {
-    parse: text => parser.parse(text),
-    astFormat: 'toml-ast'
-  }
-}
+  "aspx-parse": {
+    parse: (text) => parser.parse(text),
+    astFormat: "aspx-ast",
+  },
+};
 
-function printToml(path, options, print) {
-  const node = path.getValue()
+function printASPX(path, options, print) {
+  const node = path.getValue();
 
   if (Array.isArray(node)) {
-    return concat(path.map(print))
+    return concat(path.map(print));
   }
 
   switch (node.type) {
-    case 'Assign':
-      return concat([node.key, ' = ', path.call(print, 'value'), hardline])
-    case 'String':
-      return concat(['"', node.value, '"'])
-    case 'Integer':
-      return node.value.toString()
-    case 'Boolean':
-      return node.value.toString()
-    case 'Date':
-      return node.value.toISOString()
-    case 'ObjectPath':
-      return concat(['[', node.value.join('.'), ']', hardline])
-    case 'Array':
+    case "Assign":
+      return concat([node.key, " = ", path.call(print, "value"), hardline]);
+    case "String":
+      return concat(['"', node.value, '"']);
+    case "Integer":
+      return node.value.toString();
+    case "Boolean":
+      return node.value.toString();
+    case "Date":
+      return node.value.toISOString();
+    case "ObjectPath":
+      return concat(["[", node.value.join("."), "]", hardline]);
+    case "Array":
       return group(
         concat([
-          '[',
+          "[",
           indent(
             concat([
               softline,
-              join(concat([',', line]), path.map(print, 'value'))
+              join(concat([",", line]), path.map(print, "value")),
             ])
           ),
           softline,
-          ']'
+          "]",
         ])
-      )
+      );
     default:
-      return ''
+      return "";
   }
 }
 
 const printers = {
-  'toml-ast': {
-    print: printToml
-  }
-}
+  "aspx-ast": {
+    print: printASPX,
+  },
+};
 
 module.exports = {
   languages,
   parsers,
-  printers
-}
+  printers,
+};
